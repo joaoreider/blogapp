e086b13942f3aabb902a46ee3b5730c1bc74cba7
joaoreider <joaopauloj405@gmail.com>
2023-02-21 16:35:59 -0300

    listando categorias

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,10 @@
 
 const express = require("express")
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria =  mongoose.model('categorias')
+
 
 router.get('/', (req, res) => {
     res.render("admin/index")
@@ -11,12 +15,48 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias")
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/categorias", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Erro ao listar categorias")
+        res.redirect("/admin")
+    })
+    
 })
 
 router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias")
 })
 
+router.post('/categorias/nova', (req, res) => {
+
+    var erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if (!req.body.slug|| typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug inválido"})
+    }
+
+    if (erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }else{
+
+       const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+       }
+       new Categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "Categoria cadastrada com sucesso!")
+            res.redirect("/admin/categorias")
+       }).catch((err) => {
+            req.flash("error_msg", "Erro ao cadastrar categoria. Tente novamente")
+            res.redirect("/admin")
+       })
+
+    }
+})
 
 module.exports = router
